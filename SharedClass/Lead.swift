144755c29e7ed97f0f2e80b1d4b0a8fd2d8e4dec
李星辰 <xingchen.li@renren-inc.com>
2016-10-10 16:15:30 +0800

    fisrt commit

---
+++ SharedClass/Lead.swift
@@ -0,0 +1,13 @@
+//
+//  Lead.swift
+//  iLucky
+//
+//  Created by renren on 16/10/10.
+//  Copyright © 2016年 com.lee.lucky. All rights reserved.
+//
+
+import UIKit
+
+class Lead: NSObject {
+
+}
---
+++ SharedClass/SharedClass.h
@@ -0,0 +1,19 @@
+//
+//  SharedClass.h
+//  SharedClass
+//
+//  Created by renren on 16/10/8.
+//  Copyright © 2016年 com.lee.lucky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SharedClass.
+FOUNDATION_EXPORT double SharedClassVersionNumber;
+
+//! Project version string for SharedClass.
+FOUNDATION_EXPORT const unsigned char SharedClassVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SharedClass/PublicHeader.h>
+
+
